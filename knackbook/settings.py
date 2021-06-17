f7c4c27585cee5562c9df0d6be81d9136949eaa6
revike <revike@ya.ru>
2021-06-18 01:59:34 +0300

    static...

--- knackbook/settings.py
+++ knackbook/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'knackbook.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'mainapp/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -136,7 +136,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = (
+    BASE_DIR / "static",
+)
 
 # Медиа файлы
 MEDIA_URL = '/media/'
