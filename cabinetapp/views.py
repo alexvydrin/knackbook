25723cf6feae95b723abe5013eb2d5db1dbae65b
revike <revike@ya.ru>
2021-07-01 14:00:06 +0300

    edit register form

--- cabinetapp/views.py
+++ cabinetapp/views.py
@@ -20,7 +20,10 @@ def main(request):
         'avatar': user.avatar,
         'about_me': user.about_me,
         'gender': user.gender,
-        'birth_date': user.birth_date
+        'birth_date': user.birth_date,
+        'last_name': user.last_name,
+        'first_name': user.first_name,
+        'email': user.email
     }
 
     if request.user.is_authenticated:
