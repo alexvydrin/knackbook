c0fac2cc0f31b0860b99692e1b068c7857013a34
revike <revike@ya.ru>
2021-07-13 01:18:35 +0300

    notifications edit status and delete - js

--- notificationapp/urls.py
+++ notificationapp/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
-from notificationapp.views import notifications
+from notificationapp.views import notifications, notification_edit, \
+    notification_delete
 
 app_name = "notificationapp"
 
 urlpatterns = [
     path('', notifications, name='notification'),
+    path('notification_edit/<int:pk>/', notification_edit,
+         name='notification_edit'),
+    path('notification_delete/<int:pk>/', notification_delete,
+         name='notification_delete'),
 ]
